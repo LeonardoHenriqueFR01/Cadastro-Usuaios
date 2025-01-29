81a9f965fa43026fb8ff540aa973f0849e6693e8
Leozindev_genius <leonardohenriquefr785@gmail.com>
2025-01-28 21:07:27 -0300

    finally interface commit

--- app/routes.py
+++ app/routes.py
@@ -3,5 +3,5 @@ from flask import render_template
 
 def configure_routes(app):
     @app.route("/")
-    def index():
-        return "<h1>Bem-vindo ao Flask!</h1>"
+    def home():
+        return render_template('index.html')
