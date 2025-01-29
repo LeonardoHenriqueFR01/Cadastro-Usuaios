db581dec65988e5f0b2a9bb02897ad87ab881d5f
Leozindev_genius <leonardohenriquefr785@gmail.com>
2025-01-29 20:23:36 -0300

    Route form initial commit

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,16 @@
-from flask import render_template
+from flask import render_template, request
+from app import app
 
-
+# Rota para página principal
 def configure_routes(app):
     @app.route("/")
     def home():
         return render_template('index.html')
+
+
+
+# Rota para pegar dados de formulário
+def Formulario(app):
+    @app.route('/submit', methods=['POST'])
+    def submit():
+        pass
